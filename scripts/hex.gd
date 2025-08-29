cb48080b8d27f1d676bff3f89d0fa2fc68af1b92
ima-cow <ethanthequag@gmail.com>
2025-08-29 15:11:35 -0400

    Started working on line drawing

--- scripts/hex.gd
+++ scripts/hex.gd
@@ -1,9 +1,4 @@
 extends Sprite2D
 
-signal lineModeOn
-signal lineModeOff
 
-func _input(event):
-	if event.is_action_pressed("click") and is_pixel_opaque(get_local_mouse_position()):
-		emit_signal("lineModeOn")
  
