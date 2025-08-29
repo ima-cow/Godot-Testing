4106feb458a8e570d0dbbaba9844ebabb58a43d6
Ethan Krall <ethanthequag@gmail.com>
2025-08-29 17:01:58 -0400

    nothing again

--- scripts/hex_grid.gd
+++ scripts/hex_grid.gd
@@ -3,7 +3,7 @@ extends Node2D
 func _ready():
 	var hex = get_node("Hex")
 	for Area2D in get_children():
-		Area2D.input_event.connect(_on_hex_clicked)
+		Area2D.input_event.connect(_on_hex_input_event)
 
-func _on_hex_clicked(viewport, event, shape_idx):
+func _on_hex_input_event(viewport, event, shape_idx):
 	print("Test")
