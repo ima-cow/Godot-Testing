bfb3b18b6db84a81b09dbb1b12302aaf17d4db5e
Ethan Krall <ethanthequag@gmail.com>
2025-08-30 00:09:43 -0400

    Continued working on clicking hexs

--- scripts/hex_grid.gd
+++ scripts/hex_grid.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
+@export var redHex = Texture2D
+
 func _ready():
-	var hex = get_node("Hex")
 	for Area2D in get_children():
 		Area2D.input_event.connect(_on_hex_input_event)
 
 func _on_hex_input_event(viewport, event, shape_idx):
-	print("Test")
+	if event.is_action_pressed("click"):
+		print("click")
+		var sprite = Sprite2D.new()
+		sprite.texture = redHex
+		add_child(sprite)
+	else:
+		print("hover")
