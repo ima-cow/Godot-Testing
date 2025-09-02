740200ab1f4281ba975ddb1cd6a32a93613579ca
Ethan Krall <ethanthequag@gmail.com>
2025-09-01 22:48:48 -0400

    started making distance function

--- scripts/hex_grid.gd
+++ scripts/hex_grid.gd
@@ -3,14 +3,23 @@ extends Node2D
 @export var redHex = Texture2D
 
 func _ready():
-	for Area2D in get_children():
-		Area2D.input_event.connect(_on_hex_input_event)
+	for hex in get_children():
+		hex.input_event.connect(_on_hex_input_event)
 
 func _on_hex_input_event(viewport, event, shape_idx):
+	print(event.name)
 	if event.is_action_pressed("click"):
 		print("click")
 		var sprite = Sprite2D.new()
 		sprite.texture = redHex
+		
 		add_child(sprite)
 	else:
 		print("hover")
+
+func locate_nearest_hex():
+	var closest_hex_distance = float('inf')
+	for hex in get_children():
+		var distance_to_hex = get_global_mouse_position().distance_squared_to(hex.global_position)
+		if distance_to_hex < closest_hex_distance:
+			closest_hex_distance = distance_to_hex
