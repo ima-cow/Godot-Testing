9c871838db52c3db67542c84ddb91c3beb086e56
ima-cow <ethanthequag@gmail.com>
2025-09-04 10:17:41 -0400

    worked on generating lines

--- scripts/hex.gd
+++ scripts/hex.gd
@@ -1,4 +1,4 @@
 extends Area2D
 
-@onready var has_line = false
+@onready var has_line_segment = false
 @onready var end_point = false
--- scripts/line.gd
+++ scripts/line.gd
@@ -3,19 +3,18 @@ extends Node2D
 @onready var start = get_child(0)
 @onready var end = get_child(1)
 @export var line_texture = Texture2D
+@onready var generating = false
 
 func _ready():
 	end.get_child(1).visible = false
 	start.set_global_position(get_parent().locate_closest_hex().global_position)
-	get_parent().generate_line.connect(_on_generate_line)
-	#get_parent().locate_closest_hex().set_end_point(true)
+	generating = get_parent().generate_line.connect()
+	get_parent().locate_closest_hex().end_point = true 
 
-func _on_generate_line(line_being_generated):
-	while !line_being_generated:
-		print()
-	print("test")
-	#if line_being_genorated == false:
-		#if !get_parent().locate_closest_hex().has_line && !get_parent().locate_closest_hex().end_point:
-			#var line_segement = Sprite2D.new()
-			#line_segement.set_texture(line_texture)
-			#line_segement.set_global_position(get_parent().locate_closest_hex().get_global_position())
+func _process():
+	generate_segment()
+
+func generate_segment():
+	if generating && !get_parent().locate_cloesest_hex().end_point && !get_parent().locate_closest_hex().has_line_segment:
+		var line_segment = Sprite2D.new()
+		
