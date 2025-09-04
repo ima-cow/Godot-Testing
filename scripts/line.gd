0236c0852fec1f9e1d0296d53d7087d5728cb3fe
ima-cow <ethanthequag@gmail.com>
2025-09-04 09:24:49 -0400

    broke it with a while loop

--- scripts/line.gd
+++ scripts/line.gd
@@ -7,12 +7,15 @@ extends Node2D
 func _ready():
 	end.get_child(1).visible = false
 	start.set_global_position(get_parent().locate_closest_hex().global_position)
-	get_parent().generate_line.connect(_on_generate_list)
-	get_parent().locate_closest_hex().set_end_point(true)
+	get_parent().generate_line.connect(_on_generate_line)
+	#get_parent().locate_closest_hex().set_end_point(true)
 
-func _on_generate_list(line_being_genorated):
-	if line_being_genorated == false:
-		if !get_parent().locate_closest_hex().has_line && !get_parent().locate_closest_hex().end_point:
-			var line_segement = Sprite2D.new()
-			line_segement.set_texture(line_texture)
-			line_segement.set_global_position(get_parent().locate_closest_hex().get_global_position())
+func _on_generate_line(line_being_generated):
+	while !line_being_generated:
+		print()
+	print("test")
+	#if line_being_genorated == false:
+		#if !get_parent().locate_closest_hex().has_line && !get_parent().locate_closest_hex().end_point:
+			#var line_segement = Sprite2D.new()
+			#line_segement.set_texture(line_texture)
+			#line_segement.set_global_position(get_parent().locate_closest_hex().get_global_position())
